892af15d154e0bb2082d534cf40b4c4350fe05a3
Carlos Cuenca <cuenca.cl@gmail.com>
2020-07-05 12:55:55 -0700

    add magicalWell

--- LoopTunnel.py
+++ LoopTunnel.py
@@ -36,3 +36,26 @@ def countSumOfTwoRepresentations2(n, l, r):
         if(index <= (n - index) and (n - index) <= r): count += 1
 
     return count
+
+# --------------------------------------------------------
+# Calculates the amount of money given by the magical well
+# when casting a marble
+#
+# Problem #27
+#
+# Author: Carlos L. Cuenca
+# Date: 07/5/2020
+
+def magicalWell(a, b, n):
+    
+    salary = 0
+    
+    while n > 0:
+        
+        salary += a*b
+        a += 1
+        b += 1
+        
+        n -= 1
+        
+    return salary   
